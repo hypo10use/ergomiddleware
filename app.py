8ad694e66cac990836dc8103a4364fbd0fb4b983
Thales <oy532@york.ac.uk>
2022-02-11 22:56:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
-
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+    r=requests.post("http://116.203.30.147:9053/", data={'key': 'value'})
     return render_template('index.html')
 
 
