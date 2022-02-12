8d06f2ccde95d41546b6e0f0409aa0092488074d
Thales <oy532@york.ac.uk>
2022-02-12 22:35:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def create_round():
     ticketScript = ticketScript.replace("ticketPrice", ticketPrice)
     ticketScript = ticketScript.replace("winnerScriptHash", str(winnerScriptHash.content))
     ticketContract=requests.post("http://116.203.30.147:9053/script/p2sAddress", data=json.dumps({'source': ticketScript.replace('|', '')}), headers=headers).json()
-    ticketErgoTree=requests.get("http://116.203.30.147:9053/script/addressToTree/" + ticketContract["address"])
+    ticketErgoTree=requests.get("http://116.203.30.147:9053/script/addressToTree/" + ticketContract["error"])
     ticketScriptHash = requests.post("http://116.203.30.147:9053/utils/hash/blake2b", data=ticketErgoTree, headers=headers)
 
     scriptTokenRepo = '''{
